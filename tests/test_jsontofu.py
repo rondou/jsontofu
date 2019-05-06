9d49cd05cc8baeb0f225537ac900adce82669305
Rondou Chen <rondou@flotechnologies.com>
2019-05-06 11:30:43 +0800

    Fix PEP8 for expected 2 blank lines

--- tests/test_jsontofu.py
+++ tests/test_jsontofu.py
@@ -11,30 +11,36 @@ class Data:
     test_str: str
     test_int: int
 
+
 @dataclass
 class ListData:
     test_str: str
     test_list: List[Data]
+
 
 @dataclass
 class ListData2:
     test_str: str
     test_list: List
 
+
 @dataclass
 class DictData:
     test_str: str
     test_dict: Optional[Dict]
+
 
 @dataclass
 class OptionalData:
     test_str: str
     data: Optional[Data] = None
 
+
 @dataclass
 class NestedData:
     test_str: str
     data: Data = Any
+
 
 @dataclass
 class UserAccount:
@@ -42,6 +48,7 @@ class UserAccount:
     account_id: str
     location_id: str
     group_id: str
+
 
 @dataclass
 class Meta:
@@ -52,9 +59,11 @@ class Meta:
     args: Optional[List] = None
     kwargs: Optional[Dict] = None
 
+
 @dataclass
 class UserInfo:
     account: UserAccount
+
 
 class MetaNone:
     ty: str
@@ -83,6 +92,7 @@ class MetaNone:
         self.path = path
         self.args = args
         self.kwargs = kwargs
+
 
 @dataclass
 class MetaLostKey:
@@ -148,6 +158,7 @@ def test_no_dataclass():
     assert obj2.rtype != obj.rtype
     assert obj2.ty == "built_in"
 
+
 def test_meta():
     obj = jsontofu.decode('''{"type": "built_in", "func": "memory_info", "args": [1, 2, 3], "kwargs": {"a": "123"}, "rtype": "json", "concurrent": false}''', Meta)
     assert obj == Meta(type='built_in', func='memory_info', rtype='json', concurrent=False, args=[1, 2, 3], kwargs={'a': '123'})
@@ -158,20 +169,24 @@ def test_meta():
     obj = jsontofu.decode('''{"type": "built_in", "func": "memory_info", "args": [], "kwargs": {}, "rtype": "json", "concurrent": false}''', Meta)
     assert obj == Meta(type='built_in', func='memory_info', rtype='json', concurrent=False, args=[], kwargs=None)
 
+
 def test_dict_obj():
     obj = jsontofu.decode('''{"account": {"user_id": "12", "account_id": "34", "location_id": "56", "group_id": "78"}}''', UserInfo)
 
     assert obj == UserInfo(account=UserAccount(user_id='12', account_id='34', location_id='56', group_id='78'))
+
 
 def test_empty():
     obj = jsontofu.decode('''{}''', Data)
 
     assert obj is None
 
+
 def test_normal_dict():
     obj = jsontofu.decode('''{"test_str": "data", "test_dict": {"g": "h", "v": "w"}}''', DictData)
 
     assert obj == DictData(test_str="data", test_dict={"g": "h", "v": "w"})
+
 
 def test_nested():
     obj = jsontofu.decode('''{"test_str": "data", "data": {}}''', NestedData)
@@ -188,6 +203,7 @@ def test_nested():
     assert obj != NestedData(test_str="data")
     assert True
 
+
 def test_optional2():
     obj = jsontofu.decode('''{"test_str": "test", "data":{"test_str": "nm", "test_int": 1}}''', OptionalData)
 
@@ -200,6 +216,7 @@ def test_optional():
 
     assert obj == obj2
 
+
 def test_optional_empty():
     obj = jsontofu.decode('''{"test_str": "test", "data": {}}''', OptionalData)
     # best spec
@@ -211,6 +228,7 @@ def test_optional_empty():
     assert obj != OptionalData(test_str="test", data={})
     assert obj == OptionalData(test_str="test", data=None)
 
+
 def test_invalid():
     try:
         obj = jsontofu.decode('''{''', Data)
@@ -219,20 +237,24 @@ def test_invalid():
     else:
         assert False
 
+
 def test_int_str():
     obj = jsontofu.decode('''{"test_str": "test", "test_int": 123}''', Data)
 
     assert obj.test_str == 'test'
     assert obj.test_int == 123
 
+
 def test_list2():
     obj = jsontofu.decode('''{"test_str": "test", "test_list": ["1", 1, true]}''', ListData2)
     assert obj == ListData2(test_str="test", test_list=["1", 1, True])
+
 
 def test_list():
     obj = jsontofu.decode('''{"test_str": "test", "test_list": [{"test_str": "abc", "test_int": 11}]}''', ListData)
 
     assert obj == ListData(test_str="test", test_list=[Data(test_str="abc", test_int=11)])
+
 
 def test_type_check_str():
     try:
@@ -242,6 +264,7 @@ def test_type_check_str():
     else:
         assert False
 
+
 def test_type_check_int():
     try:
         jsontofu.decode('''{"test_str": "test", "test_list": [{"test_str": "abc", "test_int": ""}]}''', ListData)
@@ -249,6 +272,7 @@ def test_type_check_int():
         assert True
     else:
         assert False
+
 
 def test_encode():
     frozen = jsontofu.encode(jsontofu.decode('''{"test_str": "test", "test_int": 123}''', Data))
